a49d3c6b7fd6e636ae7d9e9aa7927b2a207746e3
danielc-n <npmdanielc-n@protonmail.com>
2023-02-13 18:42:55 +0100

    changes to export my package

--- esm/wrapper.mjs
+++ esm/wrapper.mjs
@@ -1,5 +1,7 @@
-import cjsModule from '../index.cjs';
-// Proskomma, lexingRegexes, blocksSpecUtils
-export const Proskomma = cjsModule.Proskomma;
-export const lexingRegexes = cjsModule.lexingRegexes;
-export const blocksSpecUtils = cjsModule.blocksSpecUtils;
+import {
+  Proskomma, lexingRegexes, blocksSpecUtils,
+} from '../index.cjs';
+
+export {
+  Proskomma, lexingRegexes, blocksSpecUtils,
+};
