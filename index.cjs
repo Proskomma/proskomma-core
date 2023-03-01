cf6b1f7e53d153aceedaab147cc48e1754335b92
danielc-n <npmdanielc-n@protonmail.com>
2023-03-01 17:00:55 +0100

    mini changes

--- index.cjs
+++ index.cjs
@@ -1,4 +1,9 @@
-const { Proskomma, lexingRegexes, resolvers, typeDefs } = require('./src/index.cjs');
+const {
+  Proskomma,
+  lexingRegexes,
+  resolvers,
+  typeDefs,
+} = require('./src/index.cjs');
 const blocksSpecUtils = require('./src/util/scriptlike/blocksSpec.cjs');
 
 module.exports = {
